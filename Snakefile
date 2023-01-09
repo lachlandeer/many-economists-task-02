e585bc7cd62753f322a7d327454a97835ae4d1de
Lachlan Deer <lachlan.deer@gmail.com>
2023-01-09 01:49:06 +0100

    script to estimate twfe regression

--- Snakefile
+++ Snakefile
@@ -5,6 +5,28 @@
 # --- Variable Declarations ---# 
 runR = "Rscript --no-save --no-restore --verbose"
 
+# --- TWFE Models --- # 
+
+rule twfe:
+    input: 
+        script      = "src/analysis/twfe.R",
+        data        = "out/data/estimation_sample.csv",
+        model_base  = "src/model-specs/twfe/twfe_main.json",
+        model_fe    = "src/model-specs/twfe/twfe_fe_simple.json",
+        model_ctrl  = "src/model-specs/twfe/twfe_controls_none.json", 
+        subset_elig = "src/data-specs/twfe/eligibility_age_enter.json",
+        subset_yrs  = "src/data-specs/twfe/years_all.json",
+        subset_hs   = "src/data-specs/twfe/school_all.json", 
+    output:
+        model = "out/analysis/twfe_testing.Rda",
+    log:
+        "log/analysis/twfe_testing.Rda"
+    shell: 
+        "{runR} {input.script} {input.data} \
+            {input.model_base} {input.model_fe} {input.model_ctrl} \
+            {input.subset_elig} {input.subset_yrs} {input.subset_hs} \
+            {output.model} > {log} 2>&1"
+
 # --- Data Cleaning --- # 
 
 rule clean_estimation_sample:
---
+++ src/analysis/twfe.R
@@ -0,0 +1,92 @@
+# twfe.R
+#
+# What this file does:
+#
+
+
+# --- Libraries ---#
+library(vroom)
+library(dplyr)
+library(fixest)
+library(rjson)
+library(rlist)
+
+# --- Command Line Unpacking --- # 
+args <- commandArgs(trailingOnly = TRUE)
+
+in_data        <- args[1]
+model_base     <- args[2]
+model_controls <- args[3]
+model_fixedeff <- args[4]
+data_elig      <- args[5]
+data_yrs       <- args[6]
+data_hs        <- args[7]
+out_file       <- args[8]
+
+# --- Load Data --- # 
+message("Loading Data")
+df <- vroom(aar) 
+
+# --- Load Model --- #
+message("Loading Regression Model")
+base     <- fromJSON(file = model_base) 
+controls <- fromJSON(file = model_controls)
+fixedeff <- fromJSON(file = model_fixedeff) 
+
+# --- Load Data Filters --- #
+message("Loading Data Filters")
+
+elig   <- fromJSON(file = data_elig)
+school <- fromJSON(file = data_hs)
+yrs    <- fromJSON(file = data_yrs)
+
+# --- Data Filtering --- #
+message("Here are the Data Filtering Criteria:")
+print(elig$KEEP_CONDITION)
+print(school$KEEP_CONDITION)
+print(yrs$KEEP_CONDITION)
+
+# Filter on Eligibility if needed
+if (elig$KEEP_CONDITION != "NULL") {
+  df <- subset(df, eval(parse(text = elig$KEEP_CONDITION)))
+}
+
+# Filter on Schooling if needed
+if (school$KEEP_CONDITION != "NULL") {
+  df <- subset(df, eval(parse(text = school$KEEP_CONDITION)))
+}
+
+# Filter on sample year if needed
+if (yrs$KEEP_CONDITION != "NULL") {
+  df <- subset(df, eval(parse(text = yrs$KEEP_CONDITION)))
+}
+
+# --- Construct Regression Formula --- # 
+# Base Regression Formula
+depvar <- base$DEPVAR 
+exog   <- base$DID
+
+reg_fmla <- as.formula(paste(dep_var, " ~ ", exog, sep=""))
+
+# Fixed Effects
+fe <- fixedeff$VARS
+reg_fmla <- paste(reg_fmla, " | ", fe, sep = "")
+
+# Controls 
+if (controls$VARS != "NULL") {
+  ctrls <- controls$VARS
+  reg_fmla <- paste(reg_fmla, " + ", ctrls, sep = "")
+}
+
+# --- Estimation --- #
+message("the regression formula is:")
+print(reg_fmla)
+
+out_model <- feols(reg_fmla, 
+                   data = df
+                )
+
+summary(out_model)
+
+# --- Export Model --- # 
+list.save(out_model, out_file)
